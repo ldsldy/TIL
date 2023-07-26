b2cf2e02301e18c188d7588687df6b05f3e89bd2
LDY <ldsldy@naver.com>
2023-07-27 01:26:25 +0900

    검색창 클릭시 포커스를 통해 고정 시키기 구현 중

--- js/hoverimg.js
+++ js/hoverimg.js
@@ -1,11 +1,33 @@
-const LinkUnderLine = document.querySelector("underline");
-const SearchBox = document.querySelector("#search_bar");
+const LinkUnderLine = document.querySelector(".underline");
+const SearchBox = document.querySelector("#search_form");
+const InputBox = document.querySelector(".search_bar_input");
 
 function drawLinkUnderLine() {
-  LinkUnderLine.opacity = 0.6;
+  LinkUnderLine.style.opacity = 0.6;
 }
+
 function hideLinkUnderLine() {
-  LinkUnderLine.opacity = 0;
+  LinkUnderLine.style.opacity = 0;
 }
-SearchBox.addEventListener("mouseover", drawLinkUnderLine);
-SearchBox.addEventListener("mouseleave", hideLinkUnderLine);
+
+// SearchBox.addEventListener("mouseover", drawLinkUnderLine);
+// SearchBox.addEventListener("mouseleave", hideLinkUnderLine);
+
+InputBox.onfocus = function () {
+  console.log("focused on input");
+  console.log(InputBox.className);
+  console.log(document.activeElement.className);
+  LinkUnderLine.style.opacity = 1;
+};
+
+InputBox.onblur = function () {
+  console.log("focused out input");
+  LinkUnderLine.style.opacity = 0;
+};
+
+if (document.activeElement.className != InputBox.className) {
+  console.log(InputBox.className);
+  console.log(document.activeElement.className);
+  SearchBox.addEventListener("mouseover", drawLinkUnderLine);
+  SearchBox.addEventListener("mouseleave", hideLinkUnderLine);
+}
--- js/search.js
+++ js/search.js
@@ -1,11 +1,16 @@
 // ch-ua-form-factor=();
-const form = document.querySelector("#search_bar");
-const SearchContent = document.querySelector("#search_bar input");
+const form = document.querySelector("#search_form");
 
 function onSearchSubmit(event) {
   event.preventDefault();
-  let Google = `https://www.google.com/search?q=${SearchContent.value}`;
+  let SearchContent = document.querySelector(".search_bar_input").value;
+  let Google = `https://www.google.com/search?q=${SearchContent}`;
   window.open(Google, "_self");
 }
+
+function onLog() {
+  console.log(SearchContent);
+}
+form.addEventListener("submit", onLog);
 
 form.addEventListener("submit", onSearchSubmit);
