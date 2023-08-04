4051b6e52f1b598c7fd17b20b5d0bb102cecbc4e
LDY <ldsldy@naver.com>
2023-08-04 23:39:40 +0900

    체크박스 체크시 취소선 기능 추가

--- js/focustoday.js
+++ js/focustoday.js
@@ -2,6 +2,9 @@ const focusTodayForm = document.querySelector(".focus_today");
 const focusTodayInput = document.querySelector(".focus_today_input");
 const focusTodayText = document.querySelector(".today_list");
 const todayContainer = document.querySelector(".today_container");
+const delCheckBox = document.querySelector(
+  ".today_container_check input[type=checkbox]"
+);
 
 const FOCUSTODAY_KEY = "focustoday";
 
@@ -38,3 +41,11 @@ if (savedFocusToday !== null) {
   focusToday = parsedFocusToday;
   parsedFocusToday.forEach(paintFocusToday);
 }
+
+delCheckBox.addEventListener("click", () => {
+  if (delCheckBox.checked) {
+    focusTodayText.style.textDecoration = "line-through";
+  } else {
+    focusTodayText.style.textDecoration = "none";
+  }
+});
