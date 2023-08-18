dd4faa4f2fb66e1c7edb184aedbb94d12a53d655
LDY <ldsldy@naver.com>
2023-08-18 22:44:47 +0900

    캔버스에 이벤트를 만들어 보기

--- 그림판/app.js
+++ 그림판/app.js
@@ -2,9 +2,28 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
-ctx.rect(0, 0, 100, 200); //(시작 위치, 크기)
-ctx.fill();
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#c56cf0",
+  "#7efff5",
+];
+
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(400, 400);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onClick);
 
 // getContext("2d")는 HTML5 Canvas 요소에 2D 컨텍스트를 가져오는 데 사용되는 메서드입니다.
 // 2D 컨텍스트는 캔버스 요소에 2D 그래픽을 그리는 데 사용되는 API입니다.
@@ -19,3 +38,4 @@ ctx.fill();
 // ctx.lineTo(50, 150);
 // ctx.lineTo(50, 50);
 // ctx.stroke();
+// ctx.arc(250, 100, 50, 0, 2 * Math.PI); //(start x, start y, diameter, startangle, endangle)
