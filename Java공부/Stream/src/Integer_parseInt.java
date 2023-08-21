58cca3ca2510720415081787342898a33e666b98
LDY <ldsldy@naver.com>
2023-08-21 23:38:21 +0900

    Integer.parseInt와 Integer.valueOf의 차이점

---
+++ Java공부/Stream/src/Integer_parseInt.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+
+public class Integer_parseInt {
+        static int solution(String n_str) {
+        int answer = Integer.parseInt(n_str);
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        String n_str="8542";
+
+        System.out.println(solution(n_str));
+
+    }
+}
