432223d5d8c4c57c134051a2c248c05b675421e0
LDY <ldsldy@naver.com>
2023-08-23 23:37:45 +0900

    마우스로 그림을 그리는 프로그램 만들기

--- 그림판/app.js
+++ 그림판/app.js
@@ -3,27 +3,25 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
-
-const colors = [
-  "#ff3838",
-  "#ffb8b8",
-  "#ff9f1a",
-  "#fff200",
-  "#32ff7e",
-  "#c56cf0",
-  "#7efff5",
-];
-
-function onClick(event) {
-  ctx.beginPath();
-  ctx.moveTo(400, 400);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+let isPainting = false;
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
-
-canvas.addEventListener("mousemove", onClick);
+function onMouseDown() {
+  isPainting = true;
+}
+function cancelPainting() {
+  isPainting = false;
+}
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
 
 // getContext("2d")는 HTML5 Canvas 요소에 2D 컨텍스트를 가져오는 데 사용되는 메서드입니다.
 // 2D 컨텍스트는 캔버스 요소에 2D 그래픽을 그리는 데 사용되는 API입니다.
@@ -39,3 +37,4 @@ canvas.addEventListener("mousemove", onClick);
 // ctx.lineTo(50, 50);
 // ctx.stroke();
 // ctx.arc(250, 100, 50, 0, 2 * Math.PI); //(start x, start y, diameter, startangle, endangle)
+//   ctx.strokeStyle = color;
