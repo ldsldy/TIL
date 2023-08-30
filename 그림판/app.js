11d925117e9868f05f07743216807b3f1631b0e3
LDY <ldsldy@naver.com>
2023-08-30 22:32:23 +0900

    지우개 기능 추가

--- 그림판/app.js
+++ 그림판/app.js
@@ -1,5 +1,7 @@
 const canvas = document.querySelector("canvas");
 const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
 const lineWidth = document.getElementById("line-width");
 const colorOptions = Array.from(
   //color-option 클래스는 여러개가 배열로 들어오므로 배열로 바꾸어 받아준다.
@@ -7,6 +9,10 @@ const colorOptions = Array.from(
 );
 const color = document.getElementById("color");
 const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
@@ -52,8 +58,17 @@ function onModeClick() {
 }
 function onCanvasPainting() {
   if (isFilling) {
-    ctx.fillRect(0, 0, 800, 800);
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   }
+}
+function onDestroyClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+function onEraserClick() {
+  ctx.strokeStyle = "white";
+  isFilling = "false";
+  modeBtn.innerText = "Fill";
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -68,6 +83,8 @@ color.addEventListener("change", onColorChange);
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 
 modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
 
 // getContext("2d")는 HTML5 Canvas 요소에 2D 컨텍스트를 가져오는 데 사용되는 메서드입니다.
 // 2D 컨텍스트는 캔버스 요소에 2D 그래픽을 그리는 데 사용되는 API입니다.
