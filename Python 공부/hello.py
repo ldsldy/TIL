b513930d7bd4ed144e813b37a489e1ceca51ddeb
LDY <ldsldy@naver.com>
2023-09-05 20:55:28 +0900

    python 개발환경 세팅 및 기초 공부

---
+++ Python 공부/hello.py
@@ -0,0 +1,6 @@
+a = 1
+b = 2
+c = "Hello World"
+print("Hello World")
+print(c)
+print(a + b)
---
+++ Python 공부/if_for.py
@@ -0,0 +1,11 @@
+a = 3
+if a > 1:
+    print("a는 1보다 큽니다.")
+
+for a in ["a", "b", "c"]:
+    print(a)
+
+i = 0
+while i < 3:
+    i = i + 1
+    print(i)
